9f0f0dfbce7c924236a8cc3851d876ad2cf4adb3
Brian Henry <BrianHenryIE@gmail.com>
2021-12-01 16:37:22 -0800

    Remove code

--- src/autoload.php
+++ src/autoload.php
@@ -15,27 +15,6 @@ namespace BrianHenryIE\WC_Search_Order_by_Total;
 
 use BrianHenryIE\WC_Search_Order_by_Total\Pablo_Pacheco\WP_Namespace_Autoloader\WP_Namespace_Autoloader;
 
-$class_map_files = array(
-	__DIR__ . '/autoload-classmap.php',
-);
-foreach ( $class_map_files as $class_map_file ) {
-	if ( file_exists( $class_map_file ) ) {
-
-		$class_map = include $class_map_file;
-
-		if ( is_array( $class_map ) ) {
-			spl_autoload_register(
-				function ( $classname ) use ( $class_map ) {
-
-					if ( array_key_exists( $classname, $class_map ) && file_exists( $class_map[ $classname ] ) ) {
-						require_once $class_map[ $classname ];
-					}
-				}
-			);
-		}
-	}
-}
-
 // Load strauss classes after autoload-classmap.php so classes can be substituted.
 require_once __DIR__ . '/strauss/autoload.php';
 
